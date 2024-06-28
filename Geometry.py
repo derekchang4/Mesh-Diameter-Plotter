[Implementation] Rotation toppling fixed: y axis reversed. Partial skeleton for mini-slices and slicing

Geometry.py
@@ -3,7 +3,7 @@
 import VectorReader as vr
 import VectorAnalyzer as va
 import VectorPlotter as vp
-import Constants as c
+import Constants as const
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 # THRESHOLD = .005 # Threshold for change in rotation in radians
 # ITERATIONNUM = 3 # The nth iteration to display
 
-THRESHOLD = c.THRESHOLD
-ITERATIONNUM = c.ITERATIONNUM
+THRESHOLD = const.THRESHOLD
+ITERATIONNUM = const.ITERATIONNUM
 
 ## This class represents a mesh geometry file and will include 
 # a set of vectors and anything else that might need to be stored.
@@ -27,17 +27,22 @@ class Mesh:
         self.ITERATIONNUM = ITERATIONNUM
 
         # Updates as the object rotates
-        self.axisIdx      = None
-        self.greatestSpan = None  # tracks the axis of greatest span and the units it spans
-        self.vectorList   = None  # required for sorting and ordering; current position
-        self.centroids    = None  # represents the centroids of the slices of the channel
-        self.datamean     = None  # represents the center of the channel
-        self.dirVector    = None  # represents the slope of the centerline
+        self.axisIdx        = None
+        self.greatestSpan   = None  # tracks the axis of greatest span and the units it spans
+        self.vectorList     = None  # required for sorting and ordering; current position
+        self.centroids      = None  # represents the centroids of the slices of the channel
+        self.datamean       = None  # represents the center of the channel
+        self.dirVector      = None  # represents the slope of the centerline
         self.totalRotation     = [0, 0, 0]  # tracks the rotation applied to the original mesh (in radians)
         self.curRotation       = [0, 0, 0]  # the amount the object was rotated by the current iteration
+        self.sorted         = False # tracks if the vectorList has been sorted
+        self.centroidsFound = False # tracks if the centroids have been computed
+        self.change         = None
+
+        # Post straightened data
+        self.slices         = None  # A vector list with points separated by slices
 
     def readVectors(self):
-        self.VECTORMAP
         if (".stl" in self.filename.lower()):
             print("[STL FILE]")
             self.VECTORMAP = vr.readVectors(self.filename)
@@ -61,33 +66,47 @@ class Mesh:
         self.greatestSpan = va.greatestSpan(self)
     
     def sortPoints(self):
+        # Don't sort if already sorted
+        if (self.sorted):
+            return
         self.vectorList = va.sortPoints(self.axisIdx, self.vectorList)
+        self.sorted = True
 
     def findCentroids(self):
+        if self.centroidsFound == True:
+            return
         self.centroids, self.vectorList = va.findCentroids(self)
         va.findCenterline(self)
+        self.sorted = True  # Finding the centroids sorts the points
+        self.centroidsFound = True
 
     def fitGeometry(self):
         self.findCentroids()
         va.fitGeometry(self)
+        self.sorted = False # Rotation mixes points
+        self.centroidsFound = False # centroids must be recomputed 
 
     # Straightens the channel iteratively until threshold in Geometry.py
     # Then calculates the diameters across the axis
-    def straighten(self, resolution):
+    def straighten(self, resolution = 1):
+        print(f"Resolution {resolution}")
         ax1 = plt.axes(projection = '3d')
         plt.title("Original")
         vp.plotMesh(self.vectorList, ax1, resolution)
         plt.show()
         iteration = 1
-        self.fitGeometry()
         print(f"\nIteration: {iteration}    threshold: {THRESHOLD}")
+        self.fitGeometry()
+        if iteration % self.ITERATIONNUM == 0:
+                plt.title(f"Iteration {iteration}")
+                self.plot(resolution)
         rotationSum = (self.curRotation[0] ** 2) + (self.curRotation[1] ** 2) + (self.curRotation[2] ** 2)
         print(f"Rotation: {self.curRotation} =  {rotationSum}")
-        while rotationSum > THRESHOLD:
+        while not self.atThreshold():
+            iteration += 1
+            print(f"\nIteration: {iteration}")
             self.fitGeometry()
             rotationSum = (self.curRotation[0] ** 2) + (self.curRotation[1] ** 2) + (self.curRotation[2] ** 2)
-            iteration += 1
-            print(f"Iteration: {iteration}")
             print(f"Rotation: {self.curRotation} =  {rotationSum}")
             print(f"Total rotation = {self.totalRotation}")
             if iteration % self.ITERATIONNUM == 0:
@@ -99,9 +118,33 @@ class Mesh:
         plt.title("Straightened")
         self.plot(resolution)
 
+    def atThreshold(self):
+        pt = self.centroids[0] # Take a point from somewhere
+        radius = np.linalg.norm(pt - self.datamean)  # Get the dist
+        c = 0
+        for axisIdx, radians in enumerate(pt):
+            c += (radius * self.curRotation[axisIdx]) ** 2
+        c = np.sqrt(c)
+        self.change = c
+        print(f"Threshold: {c} < {THRESHOLD} = {c < THRESHOLD}")
+
+        if c < THRESHOLD:
+            return True
+        else:
+            return False
+        
+    def rotate(self, theta, rotationAxis, radians = True):
+        if not radians:
+            theta = theta * np.pi / 180
+        va.rotate(self.vectorList, theta, rotationAxis, True)
+
 
 
 ### Plotting
+    def dprint(self, string, debug):
+        if debug:
+            print(string)
+
     def plotCentroids(self, ax):
         self.findCentroids()
         vp.plotCentroids(self.centroids, ax)
@@ -109,19 +152,27 @@ class Mesh:
     def plotMesh(self, ax, resolution = 1):
         vp.plotMesh(self.vectorList, ax, resolution)
 
-    def plotCenterLine(self, ax, min = -10, max= 10):
+    def plotCenterLine(self, ax, min = -10, max= 10, debug = False):
         min = self.vectorList[-1][self.axisIdx]
         max = self.vectorList[0][self.axisIdx]
-        print(f"Min: {self.vectorList[-1]}, Max: {self.vectorList[0]}")
+        self.dprint(f"Min: {self.vectorList[-1]}, Max: {self.vectorList[0]}", debug)
         vp.plotCenterLine(self.centroids, ax, min, max, self.axisIdx)
 
-    def plot(self, resolution = 1):
+    def plot(self, resolution = 1, debug = False):
         ax1 = plt.axes(projection = '3d')
         self.plotCentroids(ax1)
-        print(f"datamean: {self.datamean}")
+        self.dprint(f"\nPlotting", debug)
+        self.dprint(f"datamean: {self.datamean}", debug)
         self.plotCenterLine(ax1)
         self.plotMesh(ax1, resolution)
+        self.plotTargetAxis(ax1, self.axisIdx)
         plt.show()
+    
+    def plotTargetAxis(self, ax, axisIdx):
+        axes = [[0, 0], [0, 0], [0, 0]]
+        axes[axisIdx][1] = self.datamean[axisIdx]
+        vp.plotAxes(ax, self.datamean[axisIdx] / 4)
+        ax.plot3D(axes[0], axes[1], axes[2])
 
     def plotDiameter(self):
         ax = plt.axes()

VectorAnalyzer.py
@@ -11,7 +11,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import Constants as c
+import Constants as const
 
 # Number of slices for computing centroids
 #SLICES = 5
@@ -21,9 +21,11 @@ import Constants as c
 # Limit before the prints will skip
 #SKIPLEN = 1000
 
-SLICES = c.SLICES
-NTHTERM = c.NTHTERM
-SKIPLEN = c.SKIPLEN
+SLICES = const.SLICES
+NTHTERM = const.NTHTERM
+SKIPLEN = const.SKIPLEN
+
+MINISLICES = 2
 
 def dprint(string, debug = False):
     if (debug):
@@ -77,7 +79,7 @@ def greatestSpan(mesh):
     else:
         print("Error finding span")
         quit()
-    
+
 
 def findCentroids(mesh, debug = False):
     #Sort vectors by x, y, or z parameter
@@ -108,11 +110,8 @@ def findCentroids(mesh, debug = False):
     floor = max - sliceSize
     ceiling = max
     
-    # Iterator to get each next vertex
     # Iteratively get sums of individual x, y, and z
-    # then divide by # vertices to get
-    # the avg x, y, and z per slice
-
+    # then divide by # vertices to get the avg x, y, and z per slice
     it = iter(vectorList)
     v = next(it)
     dprint(f"Span: {mesh.greatestSpan}  SliceSize: {sliceSize}", debug)
@@ -148,7 +147,7 @@ def findCentroids(mesh, debug = False):
                 foundVertices = True
         if foundVertices:
             centroids.append(tuple(centroid))
-        dprint(f"Slice: {i}, numVertices: {numVertices}, centroid: {tuple(centroid)}", True)
+        dprint(f"Slice: {i}, numVertices: {numVertices}, centroid: {tuple(centroid)}", debug)
         #print("Iteration", i, "Floor:", floor)
         floor -= sliceSize
     # print("Min= ", min)
@@ -193,6 +192,7 @@ def slicePoints(axisIdx, vectorList):
     it = iter(vectorList)
     v = next(it)
     slices = {}
+    i = 0
     while v is not None:
         length = v[axisIdx]
         slices[length] = slices.get(length, 0) + 1
@@ -202,6 +202,56 @@ def slicePoints(axisIdx, vectorList):
             print("Finished slicing")
             v = None
     return slices
+
+def sliceMiniSlices(vectorList, axisIdx, greatestSpan):
+    # Expects a sorted list
+    centroids = []
+    
+    sliceSize = greatestSpan / SLICES
+    # List must be sorted
+    it = iter(vectorList)
+    v = next(it)
+    miniSliceSize = sliceSize / MINISLICES
+    boundary = v[axisIdx] - sliceSize
+    miniBoundary = v[axisIdx] - sliceSize
+    # For each slice
+    for i in range(SLICES):
+        miniCentroids = []
+        # Slice them into smaller slices
+        for j in range(MINISLICES):
+            numVerts = 0
+            centroid = [0, 0, 0]
+            # Keep adding points within minislice
+            while v[axisIdx] > miniBoundary:
+                centroid[0] += v[0]
+                centroid[1] += v[1]
+                centroid[2] += v[2]
+                numVerts += 1
+                try:
+                    v = next(it)
+                except:
+                    pass
+            # Get average within the minislice
+            centroid[0] = centroid[0] / numVerts
+            centroid[1] = centroid[1] / numVerts
+            centroid[2] = centroid[2] / numVerts
+            # Add this minicentroid into the current slice
+            miniCentroids.append(centroid)
+            # Shift minislice boundary down
+            if j < MINISLICES - 1:
+                miniBoundary -= miniSliceSize
+            # If at the end of a slice
+            else:
+                # Set to the boundary
+                miniBoundary = boundary
+        # If at end of mesh, set boundary below
+        if i != SLICES - 1:
+            boundary -= sliceSize
+        else:
+            boundary -= sliceSize - 1
+        # 3d list
+        centroids.append(miniCentroids)
+    return centroids
     
 # Sets a point on the centerline at the
 # same level as the lowest point on the mesh
@@ -213,6 +263,8 @@ def setToOrigin(sortedVL, dirVector, axisIdx):
 # Rotates the mesh by given number of degrees
 # about the given axis
 def rotate(vectorList, theta, axisIdx, radians= False):
+    if axisIdx == 1:
+        theta = -theta
     if radians == False:
         # Convert to radians since sin,cos expect that
         thetaRadians = theta * np.pi / 180
@@ -274,7 +326,7 @@ def fitGeometry(mesh, debug = False):
     for count, axis in enumerate(axes):
         if axis == False:
             # Get angle of rotation
-            dprint(f"Comparing axis {axisIdx} and {count}", debug)
+            dprint(f"Comparing axis {axisIdx} and {count}", True)
             dprint(f"Difference ({pt[axisIdx] - origin[axisIdx]}, {pt[count] - origin[count]})", debug)
             theta = np.arctan((pt[axisIdx] - origin[axisIdx]) / (pt[count] - origin[count]))
             rotationTheta = None
@@ -284,8 +336,6 @@ def fitGeometry(mesh, debug = False):
                 rotationTheta = (np.pi / 2) - theta
             else:
                 rotationTheta = -(np.pi / 2) - theta
-            dprint(f"{theta} radians from axis", debug)
-            dprint(f"Rotating by {rotationTheta} radians to fit", debug)
             # Mark axis
             axes[count] = True
             rotationAxis = None
@@ -293,7 +343,9 @@ def fitGeometry(mesh, debug = False):
             for idx, status in enumerate(axes):
                 if (status == False):
                     rotationAxis = idx
-                    dprint(f"Rotated about axis {idx}", debug)
+                    dprint(f"Rotated about axis {idx}", True)
+            dprint(f"Rotating by {np.pi/2} - {theta} = {rotationTheta} radians about ax[{rotationAxis}] to fit", True)
+            dprint(f"{theta} radians from horizontal", True)
             # Add to the total rotation the mesh has experienced
             mesh.totalRotation[rotationAxis] += rotationTheta       
             mesh.curRotation[rotationAxis] = rotationTheta
@@ -317,8 +369,6 @@ def findPointAlongLine(dirVector, axisIdx, datamean, axisVal = 0, debug = False)
     return foundPoint
     
 
-#! Not tested
-# may need to add datamean
 def calculateDiameter(axisIdx, sortedVL, dirVector, datamean):
     dirVector = np.array(dirVector)
     # Axis length to avg diameter at that length
@@ -343,7 +393,7 @@ def calculateDiameter(axisIdx, sortedVL, dirVector, datamean):
                 print(v, "vs", center)
             elif i % (NTHTERM * 5) == 0:
                 centers.append(center)
-                print("Shortened", v, "vs", center)
+                #print("Shortened", v, "vs", center)
             # Euclidean distance using numpy
             dist = np.linalg.norm(v - center)
             totalDiameter += dist * 2
@@ -356,6 +406,46 @@ def calculateDiameter(axisIdx, sortedVL, dirVector, datamean):
     # Returns dict of lengths along longest axis to avg diameters
     return avgDiameter, centers
         
+def calculateDiameterBySlice(axisIdx, sortedVL, dirVector, datamean, greatestSpan):
+    dirVector = np.array(dirVector)
+    # Axis length to avg diameter at that length
+    avgDiameter = {}
+    centers = []
+    slices = sliceMiniSlices(sortedVL, axisIdx, greatestSpan)
+    it = iter(sortedVL)
+    v = next(it)
+    i = 0
+
+    specifiedWidth = 5
+
+    for k in slices.items():
+        totalDiameter = 0
+        # print(f"k = {k}  v = {v}")
+        while k[0] == v[axisIdx]:
+            # scalar = v[axis] - datamean / dirVector
+            ## Wrong! -> scalar = v[axisIdx] / (dirVector[axisIdx] + datamean[axisIdx]) 
+            scalar = (v[axisIdx] - datamean) / dirVector[axisIdx]    
+            # This is how much we need to multiply
+            # the direction vector by to get to the same plane as v[axisIdx]
+            center = dirVector * scalar + datamean
+            i += 1
+            if (len(sortedVL) < SKIPLEN):
+                print(v, "vs", center)
+            elif i % (NTHTERM * 5) == 0:
+                centers.append(center)
+                #print("Shortened", v, "vs", center)
+            # Euclidean distance using numpy
+            dist = np.linalg.norm(v - center)
+            totalDiameter += dist * 2
+            try:
+                v = next(it)
+            except StopIteration:
+                print("Finished calculating diameter")
+                break
+        avgDiameter[k[0]] = totalDiameter / k[1]    # divide the diameter total by num of points
+    # Returns dict of lengths along longest axis to avg diameters
+    return avgDiameter, centers
+
 
 print("Vector Analyzer imported")
 

VectorPlotter.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import Constants as c
+import Constants as const
 import VectorAnalyzer as va
 
-NTHTERM = c.NTHTERM
+NTHTERM = const.NTHTERM
 SKIPLEN = 300
 
 def dprint(string, debug = False):
@@ -27,8 +27,8 @@ def plotCentroids(centroids, ax):
     ptsZ = np.array(ptsZ)
     ax.scatter(ptsX, ptsY, ptsZ)
 
-def plotMesh(vectorList, ax, resolution = 1):
-    print(f"Resolution {resolution}")
+def plotMesh(vectorList, ax, resolution):
+    #print(f"Resolution {resolution}")
     # VectorList should be numpy array
     ptsX = []
     ptsY = []
@@ -58,8 +58,7 @@ def plotMesh(vectorList, ax, resolution = 1):
 
 def plotCenterLine(centroids, ax, min, max, axisIdx, debug = False):
     centroids = np.array(centroids)
-    print(f"Plot centerline centroids: {centroids}")
-    print(centroids.size)
+    dprint(f"Plot centerline centroids: {centroids}", debug)
     if centroids is None or centroids.size == 0:
         print("Error: Call findCentroids() first! The direction vector, datamean, and centroids haven't been found yet!")
         quit()
@@ -79,8 +78,8 @@ def plotCenterLine(centroids, ax, min, max, axisIdx, debug = False):
     linepts = vv[0] * np.mgrid[min:max:2j][:, np.newaxis] + datamean
     dprint(f"VV[0]= {vv[0]}", debug)
     dprint(f"mgrid= {np.mgrid[min:max:2j][:, np.newaxis]}", debug)
-    dprint(f"linepts= {linepts}", True)
-    print("input to plot3d:", *linepts.T)
+    dprint(f"linepts= {linepts}", debug)
+    #print("input to plot3d:", *linepts.T)
     ax.plot3D(*linepts.T)
 
 # Takes in a dictionary of x value to avg diameter
@@ -99,6 +98,10 @@ def plotDiameter(ax, avgDiameter):
             print(f"[{v[0]} : {v[1]}]")
         i += 1
         
+def plotAxes(ax, length):
+    ax.plot3D([0, length],  [0, 0], [0, 0], c = 'r')
+    ax.plot3D([0, 0], [0, length], [0, 0], c = 'gold')
+    ax.plot3D([0, 0], [0, 0], [0, length], c = 'b')
 
 # Takes in an axes object and clears it
 def clearPlot(ax):